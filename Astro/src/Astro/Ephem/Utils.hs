0ed8153d37de5da7fb7090b03213bea1d9fee752
nigulo <nigulolspert@hotmail.com>
2015-02-11 14:23:56 +0200

    changes in clip function

--- Astro/src/Astro/Ephem/Utils.hs
+++ Astro/src/Astro/Ephem/Utils.hs
@@ -19,47 +19,31 @@ clipAngle (Rad x) = Rad $ clipAngleRad x
 clipAngle (Deg x) = Deg $ clipAngleDeg x
 
 clipAngleRad :: Double -> Double
-clipAngleRad angle
-    | angle >= 0 && angle < pi2 = angle
-    | angle < 0 =
-        let
-            (n, f) = properFraction (angle / pi2)
-        in
-            pi2 + pi2 * f
-    | otherwise = 
-        let
-            (n, f) = properFraction (angle / pi2)
-        in
-            pi2 * f
+clipAngleRad = clip pi2
 
 clipAngleDeg :: Double -> Double
-clipAngleDeg angle
-    | angle >= 0 && angle < 360 = angle
-    | angle < 0 = 
-        let
-            (n, f) = properFraction (angle / 360)
-        in
-            360 + 360 * f
-    | otherwise = 
-        let
-            (n, f) = properFraction (angle / 360)
-        in
-            360 * f
+clipAngleDeg = clip 360
 
 clipHour :: Double -> Double
-clipHour hour
-    | hour >= 0 && hour < 24 = hour
-    | hour < 0 = 
-            let
-            (n, f) = properFraction (hour / 24)
-        in
-            24 + 24 * f
-    | otherwise = 
-            let
-            (n, f) = properFraction (hour / 24)
-        in
-            24 * f
+clipHour = clip 24
 
+clip :: Double -> Double -> Double
+clip clipVal val
+    | val >= 0 && val < clipVal = val
+    | val < 0 = 
+        let
+            (n, f) = properFraction (val / clipVal)
+        in
+            clipVal + clipVal * f
+    | otherwise =
+        if isNaN val || isInfinite val 
+            then val 
+            else 
+                let
+                    (n, f) = properFraction (val / clipVal)
+                in
+                    clipVal * f
+ 
 
 calcElement :: (Double, Double) -> Double -> Double
 calcElement (elem, elemRate) ctys = elem + elemRate * ctys
