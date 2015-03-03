7a34a848ef0cad99fab2df61935a6eaa509fa16b
nigulo <nigulolspert@hotmail.com>
2015-03-03 15:25:23 +0200

    Changes in Utils.Concurrent

--- Utils/src/Utils/Concurrent.hs
+++ Utils/src/Utils/Concurrent.hs
@@ -1,71 +1,59 @@
-module Utils.Concurrent (calcConcurrently, calcConcurrently_, calcConcurrently__) where
+module Utils.Concurrent (
+    calcConcurrently, 
+    calcConcurrently_, 
+    calcConcurrently__,
+    calcConcurrently') where
 
 import Control.Concurrent.MVar
 
 import Control.Concurrent.SSem as SSem
 import Control.Concurrent
+import Control.Monad
 import Data.List
+import System.Random
 
-calcConcurrently :: (a -> (Double -> IO ()) -> IO b) -> (Double -> IO ()) -> [a] -> IO [b]
-calcConcurrently f puFunc vals = do
-    numCapabilities <- getNumCapabilities
+calcConcurrently' :: Int -> (a -> (Double -> IO ()) -> IO b) -> (Double -> IO ()) -> [a] -> IO [b]
+calcConcurrently' numCapabilities f puFunc vals = do
     let
-        numVals = length vals
-    sem <- SSem.new (-numVals + 1)
+        numValsPerThread = length vals `div` numCapabilities
+    putStrLn $ "-------" ++ show numValsPerThread ++ ", " ++ show numCapabilities
+    sem <- SSem.new (-numCapabilities + 1)
     resultsRef <- newMVar []
     pctRef <- newMVar 0
-    mapM_ (\(val, i) -> forkOn (i `mod` numCapabilities) $ 
-        do
-            result <- f val (\pct ->
-                    modifyMVar_ pctRef (\oldPct -> do
-                        if pct > oldPct 
-                            then 
-                                do
-                                    puFunc pct
-                                    return pct
-                            else
-                                return oldPct
-                        )
-                )
-            modifyMVar_ resultsRef (\results -> return (results ++ [(i, result)]))
-            SSem.signal sem
-        ) (zip vals [0 .. (length vals) - 1])
+    foldM_ (\vals i -> do
+            let
+                (initVals, tailVals) = if i < numCapabilities
+                    then
+                        splitAt numValsPerThread vals
+                    else (vals, [])
+            _ <- forkIO $ do
+                results <- mapM (\val -> do
+                        result <- f val (\pct ->
+                                modifyMVar_ pctRef (\oldPct -> do
+                                    if pct > oldPct 
+                                        then 
+                                            do
+                                                puFunc pct
+                                                return pct
+                                        else
+                                            return oldPct
+                                    )
+                            )
+                        return result
+                    ) initVals
+                modifyMVar_ resultsRef (\allResults -> return (allResults ++ [(i, results)]))
+                SSem.signal sem
+            return tailVals
+        ) vals [1 .. numCapabilities]
     SSem.wait sem
     results <- readMVar resultsRef
-    return $ map snd $ sortBy (\(i1, _) (i2, _) -> compare i1 i2) results
+    return $ concatMap snd $ sortBy (\(i1, _) (i2, _) -> compare i1 i2) results
+
+calcConcurrently :: (a -> (Double -> IO ()) -> IO b) -> (Double -> IO ()) -> [a] -> IO [b]
+calcConcurrently f puFunc vals = getNumCapabilities >>= \numCapabilities -> calcConcurrently' numCapabilities f puFunc vals  
 
 calcConcurrently_ :: (a -> IO b) -> [a] -> IO [b]
-calcConcurrently_ f vals = do
-    numCapabilities <- getNumCapabilities
-    let
-        numVals = length vals
-    sem <- SSem.new (-numVals + 1)
-    resultsRef <- newMVar []
-    mapM_ (\(val, i) -> forkOn (i `mod` numCapabilities) $ 
-        do
-            result <- f val
-            modifyMVar_ resultsRef (\results -> return (results ++ [(i, result)]))
-            SSem.signal sem
-        ) (zip vals [0 .. (length vals) - 1])
-    SSem.wait sem
-    results <- readMVar resultsRef
-    return $ map snd $ sortBy (\(i1, _) (i2, _) -> compare i1 i2) results
+calcConcurrently_ f = calcConcurrently (\x _ -> f x) (\_ -> return ())
 
 calcConcurrently__ :: (a -> b) -> [a] -> IO [b]
-calcConcurrently__ f vals = do
-    numCapabilities <- getNumCapabilities
-    let
-        numVals = length vals
-    sem <- SSem.new (-numVals + 1)
-    resultsRef <- newMVar []
-    mapM_ (\(val, i) -> forkOn (i `mod` numCapabilities) $ 
-        do
-            let
-                result = f val
-            modifyMVar_ resultsRef (\results -> return (results ++ [(i, result)]))
-            SSem.signal sem
-        ) (zip vals [0 .. (length vals) - 1])
-    SSem.wait sem
-    results <- readMVar resultsRef
-    return $ map snd $ sortBy (\(i1, _) (i2, _) -> compare i1 i2) results
-
+calcConcurrently__ f = calcConcurrently_ (\x -> return (f x))
--- Utils/tests/UtilsTests.hs
+++ Utils/tests/UtilsTests.hs
@@ -4,7 +4,7 @@ module Main where
 
 import Test.Framework
 import {-@ HTF_TESTS @-} Utils.MiscTest
-
+import {-@ HTF_TESTS @-} Utils.ConcurrentTest
 
 main :: IO()
 main = htfMain htf_importedTests
